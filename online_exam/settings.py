b3aa2a7d2aec40f8e3d1d8825e0c304ee611aa5e
5237-mests <msfnmulgeta@gmail.com>
2023-10-22 13:48:10 +0300

    localhost added to trusted url

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -104,18 +104,21 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
                         getenv("ALLOWED_H"),
                         getenv("ALLOWED_HH"),
-                        "https://frontend-eep-oe.vercel.app"]
+                        "https://frontend-eep-oe.vercel.app",
+                        "http://localhost:3000"]
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
                          getenv("ALLOWED_H"),
                          getenv("ALLOWED_HH"),
-                         "https://frontend-eep-oe.vercel.app"]
+                         "https://frontend-eep-oe.vercel.app",
+                         "http://localhost:3000"]
 
 CSRF_TRUSTED_ORIGINS = [
                         getenv("ALLOWED_H"),
                         getenv("ALLOWED_HH"),
-                        "https://frontend-eep-oe.vercel.app"]
+                        "https://frontend-eep-oe.vercel.app",
+                        "http://localhost:3000"]
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_SAMESITE = "Lax"
